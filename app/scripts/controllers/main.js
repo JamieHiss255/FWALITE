be90c55b5b917d6ce19cf8a090f629337d7d4f01
Lindsey Dohse <Lindsey Dohse>
2017-04-09 14:03:12 -0400

    More styling adjustments; correcting some typos in links

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,13 @@
 'use strict';
 
 angular.module('fwAerial')
-  .controller('HomeController', function () {
+      .controller('HomeController', ['$window', HomeController]);
 
-  });
+    function HomeController($window){
+    var vm = this; 
+    vm.navigateToMindBody = navigateToMindBody; 
+
+  function navigateToMindBody(){
+           $window.open("https://clients.mindbodyonline.com/classic/mainclass?studioid=854204", "_blank"); 
+        }
+  };

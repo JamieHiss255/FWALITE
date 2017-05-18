704907754498380c62b289f5ccb385bbd8bdb8fb
Lindsey Dohse <Lindsey Dohse>
2017-05-17 20:53:56 -0400

    Most recent changes

--- app/scripts/controllers/classes.js
+++ app/scripts/controllers/classes.js
@@ -7,8 +7,10 @@ function classesController($scope, $q, classService, $window) {
     var vm = this;
     vm.classes = [];
     vm.navigateToMindBody = navigateToMindBody;
-    var itunesLink = "https://itunes.apple.com/us/app/mindbody-book-fitness-classes-yoga-and-wellness/id689501356?mt=8";
-    var googlePlayLink = "https://play.google.com/store/apps/details?id=com.mindbodyonline.connect&hl=en&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1";
+    vm.goToAppStore = goToAppStore;
+
+    var iTunesLink = "https://www.mindbodyonline.com/get-the-app"; //"https://itunes.apple.com/us/app/mindbody-book-fitness-classes-yoga-and-wellness/id689501356?mt=8"; 
+    var googlePlayLink = "https://play.google.com/store/apps/details?id=com.mindbodyonline.connect&hl=en";
 
     loadClassList();
 
@@ -18,29 +20,14 @@ function classesController($scope, $q, classService, $window) {
         })
     }
 
-    function goToMindBodyApp() {
-        if (getMobileOperatingsystem() == "iOS")
-            return itunesLink;
-        return googlePlayLink;
-    }
-
     function navigateToMindBody() {
         $window.open("https://clients.mindbodyonline.com/classic/mainclass?studioid=854204", "_blank");
     }
 
-    function getMobileOperatingSystem() {
-        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-        if (/android/i.test(userAgent)) {
-            return "Android";
-        }
-
-        // iOS detection from: http://stackoverflow.com/a/9039885/177710
-        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-            return "iOS";
-        }
-
-        return "unknown";
+    function goToAppStore() {
+        if (navigator.userAgent.match(/iPhone|iPad|iPod/i))
+            return $window.open(iTunesLink);
+        return $window.open(googlePlayLink);
     }
 };
 
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,13 +1,46 @@
 'use strict';
 
 angular.module('fwAerial')
-      .controller('HomeController', ['$window', HomeController]);
+  .controller('HomeController', ['$window', HomeController]);
 
-    function HomeController($window){
-    var vm = this; 
-    vm.navigateToMindBody = navigateToMindBody; 
+function HomeController($window) {
+  var vm = this;
+  var iTunesLink = "https://www.mindbodyonline.com/get-the-app"; //"https://itunes.apple.com/us/app/mindbody-book-fitness-classes-yoga-and-wellness/id689501356?mt=8"; 
+  var googlePlayLink = "https://play.google.com/store/apps/details?id=com.mindbodyonline.connect&hl=en"; 
 
-  function navigateToMindBody(){
-           $window.open("https://clients.mindbodyonline.com/classic/mainclass?studioid=854204", "_blank"); 
-        }
+  vm.navigateToMindBody = navigateToMindBody;
+  vm.goToAppStore = goToAppStore; 
+
+  function navigateToMindBody() {
+    $window.open("https://clients.mindbodyonline.com/classic/mainclass?studioid=854204", "_blank");
+  }
+
+  var isMobile = {
+    Android: function () {
+      return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+      return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+      return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+      return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+      return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+
   };
+
+  function goToAppStore() {
+    if (navigator.userAgent.match(/iPhone|iPad|iPod/i))
+      return $window.open(iTunesLink);
+    return $window.open(googlePlayLink);
+  }
+
+};
